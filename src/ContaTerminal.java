da9f748775d46c6312898a98ec6e2321f8607f3f
Raniel Luna <raniielluna@gmail.com>
2024-07-03 14:42:37 -0300

    metodo para criar conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,26 @@
- public class ContaTerminal {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Hello and welcome!");
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o seu Nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        scanner.close();
     }
 }
